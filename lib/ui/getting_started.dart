ac3464245cd4d522447cf949e8a7211e9decfcb2
Waffiq Muchlishin <waffiqw999@gmail.com>
2025-07-26 00:21:08 +0700

    setting main, buat file, nambahin font

---
+++ lib/ui/getting_started.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GettingStartedPage extends StatelessWidget {
+  const GettingStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(90, 157, 255, 1),
+      body: Center(
+        child: Column(
+          
+        )
+      )
+    );
+  }
+}
