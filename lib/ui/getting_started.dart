a9c7c8c72e33aacd2426bd820bf9ba5da976f3d0
Waffiq Muchlishin <waffiqw999@gmail.com>
2025-07-26 10:15:27 +0700

    image

--- lib/ui/getting_started.dart
+++ lib/ui/getting_started.dart
@@ -9,7 +9,14 @@ class GettingStartedPage extends StatelessWidget {
       backgroundColor: const Color.fromRGBO(90, 157, 255, 1),
       body: Center(
         child: Column(
-          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'lib/assets/image/logo-hypcare.png',
+              width: 150,
+              height: 150,
+            )
+          ]
         )
       )
     );
