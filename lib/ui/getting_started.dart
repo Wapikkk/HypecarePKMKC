54890f67a8f2821d33ee297c6275655f21b00dba
Waffiq Muchlishin <waffiqw999@gmail.com>
2025-07-26 11:05:26 +0700

    design getting started

--- lib/ui/getting_started.dart
+++ lib/ui/getting_started.dart
@@ -12,10 +12,51 @@ class GettingStartedPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
-              'lib/assets/image/logo-hypcare.png',
-              width: 150,
-              height: 150,
-            )
+              'assets/image/logo-hypecare.png',
+              width: 190,
+              height: 190,
+            ),
+            const SizedBox(height: 0),
+            Text(
+              'Hypecare',
+              style: TextStyle (
+                fontFamily: 'Nunito',
+                fontSize: 40,
+                fontWeight: FontWeight.w800,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 0),
+            Text(
+              'Monitoring & Treatment',
+              style: TextStyle(
+                fontFamily: 'Inika',
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 150),
+            ElevatedButton(
+              onPressed: () {
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: const Color.fromRGBO(90, 157, 255, 1),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+              ),
+              child: const Text(
+                'Mulai',
+                style: TextStyle(
+                  fontFamily: 'inika',
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ]
         )
       )
